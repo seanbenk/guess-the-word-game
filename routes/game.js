ddad74c653d33f980ea414b48cf57aa12444b8b4
seanbenkenstein <seanbenkenstein@gmail.com>
2022-02-12 22:39:42 +1100

    changed the view for /game route to take game.html instead of just game

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET game page. */
 router.get("/", function (req, res, next) {
-  res.render("game", { title: "Random Word Guess Game" });
+  res.render("game.html", { title: "Random Word Guess Game" });
 });
 
 module.exports = router;
