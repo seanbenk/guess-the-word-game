d386394602e4764094b209bac3f6b0fbaeef084d
seanbenkenstein <seanbenkenstein@gmail.com>
2022-02-12 22:43:07 +1100

    few refactors to html documents

--- routes/game.js
+++ routes/game.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET game page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("game.html", { title: "Random Word Guess Game" });
 });
 
