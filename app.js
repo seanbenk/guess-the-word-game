42d38eea10c5423e11f2f5cfacd98b10c55be405
seanbenkenstein <seanbenkenstein@gmail.com>
2022-02-12 14:06:17 +1100

    cors added to the app

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 const indexRouter = require("./routes/index");
 const wordsRouter = require("./routes/words");
@@ -12,6 +19,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
