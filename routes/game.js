752703ac138d4b25c4b0bac996dfdcd9cf922f79
seanbenkenstein <seanbenkenstein@gmail.com>
2022-02-12 22:49:17 +1100

    removed .html from game file in game view

--- routes/game.js
+++ routes/game.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET game page. */
 router.get("/", (req, res, next) => {
-  res.render("game.html", { title: "Random Word Guess Game" });
+  res.render("game", { title: "Random Word Guess Game" });
 });
 
 module.exports = router;
